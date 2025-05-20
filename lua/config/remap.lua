c9e99b577370287365e3dc11feba96b598e99897
Tom Delaney <delaneyt1@gmail.com>
2025-05-20 19:18:42 -0400

    some remap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,3 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration)
