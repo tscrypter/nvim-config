392f960219e794f64c7145c87077995fa67f075c
Tom Delaney <delaneyt1@gmail.com>
2025-05-20 19:34:51 -0400

    add noice

--- lua/plugins/folke.lua
+++ lua/plugins/folke.lua
@@ -78,8 +78,8 @@ return {
   -- { dir = "~/projects/secret.nvim" },
 
   -- you can use a custom url to fetch a plugin
-  { url = "git@github.com:folke/noice.nvim.git" },
-
+  -- { url = "git@github.com:folke/noice.nvim.git" },
+  --
   -- local plugins can also be configured with the dev option.
   -- This will use {config.dev.path}/noice.nvim/ instead of fetching it from GitHub
   -- With the dev option, you can easily switch between the local and installed version of a plugin
